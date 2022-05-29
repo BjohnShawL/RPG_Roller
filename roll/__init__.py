143848c0e4a60208b1c5b5cecf0e3e118072aa90
Ben <bjohnshawl@gmail.com>
2022-05-29 11:09:55 +0100

    Added modified results dict to API return

--- roll/__init__.py
+++ roll/__init__.py
@@ -11,6 +11,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     def handle_single_roll(sides :int, modifier: int = 0, neg: bool = False):
         result = randint(1,sides)
         modded = result + modifier if not neg else result - modifier
+        result_dict["results"].append(modded)
         if modifier > 0 :
             detail =f"{result} {'+' if not neg else '-'} {modifier }"
         else:
@@ -31,7 +32,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             sides = dice.get("sides")
             modifier = dice.get("mod")
             neg = dice.get("neg",False)
-            result_dict = {"sum":0, "detail":[]}
+            result_dict = {"sum":0, "results":[], "detail":[]}
             for _ in range(quantity):
                 handle_single_roll(sides=sides,modifier=modifier, neg=neg)
             results_list.append(result_dict)
